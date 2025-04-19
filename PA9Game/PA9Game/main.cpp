731b594e47bbef47f71233bc43c9143e3fe21253
AidanLarsen <aidan.larsen@wsu.edu>
2025-04-19 14:10:25 -0700

    Creation of Objects

--- PA9Game/PA9Game/main.cpp
+++ PA9Game/PA9Game/main.cpp
@@ -1,10 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include "Object.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({ 1000, 800 }), "SFML works!");
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
+
+    std::vector<GameObject> objects;
+    objects.emplace_back(sf::Vector2f{ 1000, 50 }, sf::Vector2f{ 0, 750 }, sf::Color::Green);
+    objects.emplace_back(sf::Vector2f{ 100, 50 }, sf::Vector2f{ 200, 550 }, sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -15,7 +20,10 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        for (const auto& object : objects)
+        {
+            window.draw(object.shape);
+        }
         window.display();
     }
 }
