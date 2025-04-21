d90a2d2a4a8cf1f80e6f17973bc71fc540b301cf
xeno <troymuehlbauer1@gmail.com>
2025-04-20 19:07:16 -0700

    basic backdrop and prop file
    prop if we want things like animated trees, clouds

--- PA9Game/PA9Game/main.cpp
+++ PA9Game/PA9Game/main.cpp
@@ -1,15 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include "Object.hpp"
 #include "Entity.hpp"
+#include "Backdrop.hpp"
 
 int main()
 {
     
 
-    sf::RenderWindow window (sf::VideoMode({ 256, 420}), "SFML works!");
+    sf::RenderWindow window (sf::VideoMode({ 1536u, 1024u}), "SFML works!");
 
     const std::string character = "Sprite.png";
-  
+    Backdrop test1("Backdrop1.png", "Ground.png");
     Entity test(character);
 
     while (window.isOpen())
@@ -23,6 +24,7 @@ int main()
         
         window.clear();
         test.setScale(0.2, 0.2);
+        test1.drawBackdrop(window);
         test.drawEntity(window);
         window.display();
 
